716946c98dd61c667ba1178dbbc32af18246d9dc
Xiao Ling <lingxiao@seas.upenn.edu>
2016-09-06 14:08:57 -0400

    first script

---
+++ nlpgridscripts/scipt1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# $HOME/sample_applications/sample_sge_script_1.sh
+
+# wallclock time reservation (format is hours:minutes:seconds).
+# man 5 complex
+#$ -l h_rt=0:10:0
+
+# request 1 gigabyte of RAM 
+# man 5 complex
+#$ -l mem=1G
+
+# name of job
+# man 1 qsub
+#$ -N sample_sge_script
+
+# working directory (check for specific requirements for your research group)
+# man 1 qsub
+#$ -wd /scratch/username/sample_script_outputs/
+
+# make sure I set my $CWD (current working directory)
+cd $HOME/nlpgridscripts
+
+
+# when am I running
+/bin/date
+
+# where am I running
+/bin/hostname
+
+# what environment variables are available to this job script, e.g. $JOB_ID
+/usr/bin/env
