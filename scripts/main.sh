2f8e57509c8e0886a58c701e46f55a4009ba5715
Xiao Ling <lingxiao@seas.upenn.edu>
2016-09-10 10:19:03 -0400

    rm error

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,11 +5,6 @@ set -x
 
 # $HOME/sample_applications/sample_sge_script_1.sh
 
-# set logs path
--o $PWD/logs
--e $PWD/logs
-
-
 
 # wallclock time reservation (format is hours:minutes:seconds).
 # man 5 complex
